2912546c5b02f6fe0728e0f5c52ced706594494c
Amir-Karim <amir.karim046@gmail.com>
2023-03-22 16:34:36 +0530

    First_Commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java this is my first project");
+        System.out.println("This is on java 11 version check the difference");
+        System.out.println("check jdk 11 version.");
+        System.out.println("I am adding this programm to my git account ");
+
+    }
+}
